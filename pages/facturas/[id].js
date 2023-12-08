afc75c069cfa4f4b98faed7dac8b8c5992697eda
Martina Ibañez <martinaiwulbrandt@gmail.com>
2023-12-07 21:12:31 -0300

    Delete pages directory

--- pages/facturas/[id].js
+++
@@ -1,5 +0,0 @@
-// const facturas = [
-//     { id: 1, cliente: 'Jorge Embarrado', importe: 45000 },
-//     { id: 2, cliente: 'Guillermo Barros', importe: 7000 },
-//   ];
-// export default facturasIdes;  
